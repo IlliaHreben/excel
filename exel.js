075eff955455b740bb9b05fc90506fe17afff8f7
Illia Hreben <IlliaHreben@gmail.com>
2020-11-24 22:35:10 +0200

    add eslint, format eslint, add libre convert (wip)

--- exel.js
+++ exel.js
@@ -1,113 +1,116 @@
-const fs = require('fs').promises
-const childProcess = require('child_process')
-const path = require('path')
-const Excel = require('exceljs')
-const os = require('os')
+/* eslint-disable max-len */
+/* eslint-disable no-console */
+const fs = require('fs').promises;
+// const childProcess = require('child_process');
+const path = require('path');
+const Excel = require('exceljs');
+const os = require('os');
+const libre = require('libreoffice-convert');
 
+const xlsToXlsx = async (xlsPath, xlsxPath) => {
+  const file = await fs.readFile(xlsPath);
 
-const xlsToXlsx = (xlsPath, xlsxPath) => {
+  await libre.convert(file, '.xlsx', undefined, async (err, data) => {
+    if (err) {
+      console.log(`Error converting file: ${err}`);
+    }
 
-  const excelcnvPath = path.join('C:', 'Program Files', 'Microsoft Office', 'root', 'Office16', 'excelcnv.exe')
-  const arguments = [ '-oice', xlsPath, xlsxPath ]
+    // Here in done you have pdf file which you can save or transfer in another stream
+    const result = await fs.writeFile(xlsxPath, data);
+    return result;
+  });
 
-  return new Promise((resolve, reject) => {
-    childProcess.execFile(
-      excelcnvPath,
-      arguments,
-      (err, out, stdErr) => {
-        // console.log(out, stdErr, xlsPath);
-        err ? reject(err) : resolve()
-      }
-    )
-  })
-}
+  // const excelcnvPath = path.join('C:', 'Program Files', 'Microsoft Office', 'root', 'Office16', 'excelcnv.exe')
+  // const arguments = [ '-oice', xlsPath, xlsxPath ]
 
-function readDataFromExcel(personDirPath) {
+  // return new Promise((resolve, reject) => {
+  //   childProcess.execFile(
+  //     excelcnvPath,
+  //     arguments,
+  //     (err, out, stdErr) => {
+  //       // console.log(out, stdErr, xlsPath);
+  //       err ? reject(err) : resolve()
+  //     }
+  //   )
+  // })
+};
 
+async function readDataFromExcel(personDirPath) {
   return fs.readdir(personDirPath)
-    .then(xlsFilesNames => {
-      return fs.mkdtemp(path.join(os.tmpdir(), 'XlsxFiles'))
-        .then(xlsxFilesDirPath => {
-          return xlsFilesNames.map(xlsFileName => [
-            path.resolve(personDirPath, xlsFileName),
-            path.join(xlsxFilesDirPath, xlsFileName + 'x')
-          ])
-        })
-    })
-    .then(pathes => {
-      return pathes.reduce((acc, [xlsPath, xlsxPath]) => {
-        return acc.then(() => xlsToXlsx(xlsPath, xlsxPath))
-      }, Promise.resolve())
-        .then(() => pathes.map(([, xlsxPathes]) => xlsxPathes))
-    })
-    .then(xlsxPathes => {
-      return Promise.all(
-        xlsxPathes.map(xlsxPath => {
-
-          const workbook = new Excel.Workbook();
-          return workbook.xlsx.readFile(xlsxPath)
-            .then(() => {
-              const [, , , ...timestamps] = workbook.getWorksheet(1).getColumn(1).values
-              const [, , , ...values] = workbook.getWorksheet(1).getColumn(2).values
-              const measurements = timestamps.map((timestamp, i) => [timestamp, values[i]])
-              return {measurements, xlsxPath}
-            })
-
-        })
+    .then((xlsFilesNames) => fs.mkdtemp(path.join(os.tmpdir(), 'XlsxFiles'))
+      .then((xlsxFilesDirPath) => xlsFilesNames.map((xlsFileName) => [
+        path.resolve(personDirPath, xlsFileName),
+        path.join(xlsxFilesDirPath, `${xlsFileName}x`),
+      ])))
+    .then((pathes) => pathes
+      .reduce(
+        (acc, [xlsPath, xlsxPath]) => acc.then(async () => {
+          const result = await xlsToXlsx(xlsPath, xlsxPath);
+          return result;
+        }),
+        Promise.resolve(),
       )
-    })
+      .then(() => pathes.map(([, xlsxPathes]) => xlsxPathes)))
+    .then((xlsxPathes) => Promise.all(
+      xlsxPathes.map((xlsxPath) => {
+        const workbook = new Excel.Workbook();
+        return workbook.xlsx.readFile(xlsxPath)
+          .then(() => {
+            const [, , , ...timestamps] = workbook.getWorksheet(1).getColumn(1).values;
+            const [, , , ...values] = workbook.getWorksheet(1).getColumn(2).values;
+            const measurements = timestamps.map((timestamp, i) => [timestamp, values[i]]);
+            return { measurements, xlsxPath };
+          });
+      }),
+    ));
 }
 
-function setCell(worksheet, cell, value) {
-  worksheet.getCell(cell).value = value
-}
-
+// function setCell(worksheet, cell, value) {
+//   worksheet.getCell(cell).value = value;
+// }
 
 function writeIterationsToExcel(data, personDirPath) {
-  const workbook = new Excel.Workbook()
+  const workbook = new Excel.Workbook();
 
-  for (const {iterations, xlsxPath} of data) {
-    const sheetName = path.parse(xlsxPath).name
-    const sheet = workbook.addWorksheet(sheetName)
+  data.forEach(({ iterations, xlsxPath }) => {
+    const sheetName = path.parse(xlsxPath).name;
+    const sheet = workbook.addWorksheet(sheetName);
 
     iterations.forEach((iteration, i) => {
+      const { sum, max } = iteration.reduce((acc, [time, value]) => {
+        sheet.addRow([i + 1, time, value]);
+        acc.sum += value;
+        if (value > acc.max) acc.max = value;
+        return acc;
+      }, { sum: 0, max: 0 });
 
-      const {sum, max} = iteration.reduce((acc, [time, value]) => {
-        sheet.addRow([i+1, time, value])
-        acc.sum += value
-        if (value > acc.max) acc.max = value
-        return acc
-      }, {sum: 0, max: 0})
+      const { variance, rawMad, wamp } = iteration.reduce((acc, [, value], k, arr) => {
+        acc.variance += (((sum / arr.length) - value) ** 2) / (arr.length - 1); // variance
+        acc.rawMad += Math.abs((sum / arr.length) - value); // rawMad
+        if (arr[k + 1] && Math.abs(value - arr[k + 1][1]) >= 10) acc.wamp += 1; // wamp
 
-      const {variance, rawMad, wamp} = iteration.reduce((acc, [ , value], i, arr) => {
+        return acc;
+      }, { variance: 0, rawMad: 0, wamp: 0 });
 
-        acc.variance += ((Math.pow((sum / arr.length) - value, 2)) / (arr.length - 1))  //variance
-        acc.rawMad += Math.abs((sum / arr.length) - value)   // rawMad
-        if (arr[i+1] && Math.abs(value - arr[i+1][1]) >= 10) acc.wamp += 1  // wamp
+      const mad = (1 / iteration.length) * rawMad;
 
-        return acc
-      }, {variance: 0, rawMad: 0, wamp: 0})
+      sheet.getCell(`E${i + 2}`).value = i + 1;
+      sheet.getCell(`F${i + 2}`).value = sum;
+      sheet.getCell(`G${i + 2}`).value = max;
+      sheet.getCell(`H${i + 2}`).value = variance;
+      sheet.getCell(`I${i + 2}`).value = mad;
+      sheet.getCell(`J${i + 2}`).value = wamp;
+    });
 
-      const mad = (1 / iteration.length) * rawMad
+    sheet.getCell('E1').value = 'IterNum';
+    sheet.getCell('F1').value = 'Sum';
+    sheet.getCell('G1').value = 'Max';
+    sheet.getCell('H1').value = 'Var';
+    sheet.getCell('I1').value = 'Mad';
+    sheet.getCell('J1').value = 'WAMP';
+  });
 
-      sheet.getCell('E' + (i+2)).value = i+1
-      sheet.getCell('F' + (i+2)).value = sum
-      sheet.getCell('G' + (i+2)).value = max
-      sheet.getCell('H' + (i+2)).value = variance
-      sheet.getCell('I' + (i+2)).value = mad
-      sheet.getCell('J' + (i+2)).value = wamp
-    })
-
-    sheet.getCell('E1').value = 'IterNum'
-    sheet.getCell('F1').value = 'Sum'
-    sheet.getCell('G1').value = 'Max'
-    sheet.getCell('H1').value = 'Var'
-    sheet.getCell('I1').value = 'Mad'
-    sheet.getCell('J1').value = 'WAMP'
-  }
-
-   return workbook.xlsx.writeFile(path.resolve(personDirPath + '.xlsx'))
+  return workbook.xlsx.writeFile(path.resolve(`${personDirPath}.xlsx`));
 }
 
-
-module.exports = {readDataFromExcel, writeIterationsToExcel}
+module.exports = { readDataFromExcel, writeIterationsToExcel };
